04ed7a58b82da842673ce1b98c415486cfb5bb0a
Emir Stiles <70492089+emir-stiles@users.noreply.github.com>
2020-08-30 21:59:29 -0500

    parte 2 del ejercicio 2
    ejercicio que habla sobre el numero par y si es multiplo de 4

--- ejercicio_2_par_o_impar.py
+++ ejercicio_2_par_o_impar.py
@@ -1,6 +1,9 @@
 numero = int(input("escribe un numero "))
 mod = numero % 2 
-if mod == 0:
-    print("es un numero par")
+mod4 = numero % 4 
+if mod == 0 and mod4 == 0:
+    print("el numero es multiplo de 2 y 4")
+elif mod == 0 and mod4 != 0:
+        print("el numero solo es multiplo de 2")
 else : 
         print("es un numero impar")
